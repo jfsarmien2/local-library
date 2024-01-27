dc130b7d5a003672f1688b74f22d88ddd17f087c
John <john.sarmiento452@gmail.com>
2024-01-27 15:24:43 +0800

    create homepage

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from .models import Book, BookInstance, Author, Genre
 # Create your views here.
+
+
+def index(request):
+
+    num_of_books = Book.objects.all().count()
+    num_of_instance = BookInstance.objects.all().count()
+
+    num_of_instance_available = BookInstance.objects.filter(
+        status__exact='a').count()
+
+    num_of_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_of_books,
+        'num_instances': num_of_instance,
+        'num_instances_available': num_of_instance_available,
+        'num_authors': num_of_authors,
+    }
+
+    return render(request, 'index.html', context)
